35f302299ebb170f01a2c43fcf5e572721d7484d
albinholmberg <alho1901@student.miun.se>
2022-04-04 14:26:47 +0200

    Initial Commit

---
+++ Company2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Company2
+//
+//  Created by albin holmberg on 2022-04-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
